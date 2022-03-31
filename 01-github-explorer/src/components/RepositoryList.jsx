886910e8a127abef5cb096bca306e58d7ac78ec0
Matheus Bertemes <mathbertemes@gmail.com>
2022-03-31 18:43:56 -0300

    using useEffect to set api info

--- 01-github-explorer/src/components/RepositoryList.jsx
+++ 01-github-explorer/src/components/RepositoryList.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+
 import { RepositoryItem } from "./RepositoryItem";
 import "../styles/repositories.scss"
 const repository = {
@@ -5,8 +7,17 @@ const repository = {
   description : 'Forms in React',
   link : 'https://www.youtube.com/'
 }
+//https://api.github.com/orgs/rocketseat/repos
 
 export function RepositoryList(){
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/orgs/rocketseat/repos")
+    .then(response => response.json())
+    .then(data => setRepositories(data))
+  }, [])
+
   return(
     <section className="repository-list">
       <h1>Lista de repositorios</h1>
