a561f2af9abd0af42752f56baa1853e52ac2779d
mathbertmes <mathbertemes@gmail.com>
2022-05-12 17:06:26 -0300

    TransitionsTable Component

--- dtmoney/src/components/Dashboard/index.tsx
+++ dtmoney/src/components/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import { Summary } from "../Summary";
+import { TrasactionsTable } from "../TransactionsTable";
 import * as S from "./styles";
 
 export function Dashboard() {
   return (
     <S.Container>
       <Summary />
+      <TrasactionsTable />
     </S.Container>
   );
 }
